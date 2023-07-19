1d798c0211a397f3351755a652c1c89fdb253a14
MAYINUDDIN <mayindiu@gmail.com>
2023-07-19 22:02:51 +0600

    first commit

---
+++ src/pages/Darkteam.js
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+const Darkteam = () => {
+
+    return (
+        <div>
+            <Button sx={{ mr: 2}} color="success" variant="contained">Fahad vai</Button>
+            <Button sx={{ mr: 2}} variant="contained">Mayin</Button>
+           <Button sx={{ mr: 2}} variant="contained">Burhan</Button>
+           <Button sx={{ mr: 2}} variant="contained">Alamin</Button>
+           <Button sx={{ mr: 2}} variant="contained">Khurshed</Button>
+        </div>
+    );
+};
+
+export default Darkteam;
